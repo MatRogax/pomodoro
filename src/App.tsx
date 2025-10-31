06acf67def53ef060f97c04600ea99bf75693ed9
Matheus Rogato <matheusrogato@gmail.com>
2025-10-31 19:53:30 -0300

    feat(Heading): criacao e estilizacao do componente header do pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './styles/global.css';
+import { Heading } from './components/Heading';
+import './styles/theme.css';
+import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => (
+  <>
+    <Heading>
+      hello world
+      <button>
+        <TimerIcon />
+      </button>
+    </Heading>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <p>lore impum bla bla bla bla</p>
+  </>
+);
 
-export default App
+export default App;
